ece927dd1196443798cfa79225ae5274e80d673b
Nicolas Aguttes <nicolas.aguttes@gmail.com>
2017-03-17 07:29:05 -0700

    adding reactroute and alt

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,13 @@ module TestReact
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.react.server_renderer_options = {
+      files: ["react-server.js", "alt_init.js", "routes_init.js", "prerender.js"], # files to load for prerendering
+      replay_console: true,                 # if true, console.* will be replayed client-side
+    }
+     
+    #reactjs
+    config.react.addons = true
   end
 end
